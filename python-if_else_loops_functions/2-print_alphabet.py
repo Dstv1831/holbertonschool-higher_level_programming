06ff07491231ec059544a8ebc4af8005b519360d
DSTV <9871@holbertonstudents.com>
2025-01-13 18:01:25 +1100

    Task Complete

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,6 +2,5 @@
 
 """Print the alphabet in lowercase, not followed by a new line."""
 
-for n in range (ord('a'),ord('z') + 1):
+for n in range(ord('a'), ord('z') + 1):
     print("{0}".format(chr(n)), end='')
-    
