ef4393ff7060ffcec609005a1465d8c2438d878d
DSTV <9871@holbertonstudents.com>
2025-01-14 10:36:59 +1100

    Task Complete

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-""" Write a program that prints the ASCII alphabet,
-except for 'e' and 'q', in lowercase, not followed by a new line. """
-
 for n in range(ord('a'), ord('z') + 1):
     if n == ord('q') or n == ord('e'):
         continue
