dea6dc7d1f046d9f55e3f6456c854bea8331d901
DSTV <9871@holbertonstudents.com>
2025-01-14 11:39:20 +1100

    Task Complete

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,9 @@
 
 u = 0
 while u < 8:
-    for d in range(0,10):
-        if u == d or u > d :
+    for d in range(0, 10):
+        if u == d or u > d:
             continue
         print("{0}{1}, ".format(u, d), end="")
     u += 1
 print("{0}{1}".format(u, d))
-
