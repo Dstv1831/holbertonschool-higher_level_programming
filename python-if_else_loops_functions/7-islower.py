b95347a7c342ef692eb468386c98f599d22e435e
DSTV <9871@holbertonstudents.com>
2025-01-14 11:53:09 +1100

    Task Complete

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,8 +3,8 @@
 """ Write a function that checks for lowercase character."""
 
 def islower(c):
-    if 97 < ord(c) < 123:
-        return (True)
+    if 96 < ord(c) < 123:
+        return(True)
     else:
-        return (False)
+        return(False)
 
