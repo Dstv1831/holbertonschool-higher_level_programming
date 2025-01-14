0c490e57069c2d6e2a0d7bf0a80b29024d623f7e
DSTV <9871@holbertonstudents.com>
2025-01-14 11:23:14 +1100

    Task Complete

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+n = 0
+while n < 99:
+    if 0 <= n < 10:
+        n = str(n).zfill(2)
+    print(f"{n}, ", end="")
+    n = int(n)
+    n += 1
+print(f"{n}")
---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+u = 0
+while u < 8:
+    for d in range (0,10):
+        if u == d or :
+            continue
+        print(f"{u}{d}, ", end="")
+    u += 1
+print(f"{n}")
