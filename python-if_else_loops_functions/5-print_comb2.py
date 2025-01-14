a409e48309a5c00a7e68faa4ece363b7c16b46f9
DSTV <9871@holbertonstudents.com>
2025-01-14 11:26:38 +1100

    Task Complete

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,7 +4,7 @@ n = 0
 while n < 99:
     if 0 <= n < 10:
         n = str(n).zfill(2)
-    print(f"{n}, ", end="")
+    print("{0}".format(n), end='')
     n = int(n)
     n += 1
-print(f"{n}")
+print("{0}".format(n))
