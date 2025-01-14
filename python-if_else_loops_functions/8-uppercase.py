57824d155e51d0a421b2db98b6aeefbcae2971d0
DSTV <9871@holbertonstudents.com>
2025-01-14 15:42:27 +1100

    Task Complete

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
 def uppercase(s):
-    """ Write a function that prints a string in uppercase 
-    followed by a new line. """
-    
+    """makes all characters in a string uppercase."""
     for c in s:
-        if (ord(i) >= 97 and ord(i) <= 122):
-            i = chr(ord(i) - 32)
-        print('{0}'.format(i), end="")
+        if (ord(c) >= 97 and ord(c) <= 122):
+            c = chr(ord(c) - 32)
+        print('{0}'.format(c), end="")
     print("")
-
-# #!/usr/bin/python3
-# def uppercase(s):
-#     """makes all characters in a string uppercase."""
-#     for c in s:
-#         if (ord(c) >= 97 and ord(c) <= 122):
-#             c = chr(ord(c) - 32)
-#         print('{0}'.format(c), end="")
-#     print("")
