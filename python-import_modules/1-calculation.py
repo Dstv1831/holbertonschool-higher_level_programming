76bcd8b9c56b5b79a164f4fd35a251df19b8cffe
DSTV <9871@holbertonstudents.com>
2025-01-14 17:02:51 +1100

    Task Complete

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -9,4 +9,3 @@ print("{0} + {1} = {2}".format(a, b, add(a, b)))
 print("{0} - {1} = {2}".format(a, b, sub(a, b)))
 print("{0} * {1} = {2}".format(a, b, mul(a, b)))
 print("{0} / {1} = {2}".format(a, b, div(a, b)))
-
