d6b3724bf8e5d04d09cf54c69666617e948e15fe
DSTV <9871@holbertonstudents.com>
2025-01-14 15:30:23 +1100

    Task Complete

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -8,5 +8,5 @@ def uppercase(str):
         asci = ord(i)
         if 96 < asci < 123:
             i = chr(asci - 32)
-        print("{0}".format(i), end = '')
+        print("{0}".format(i), end="")
     print("")
