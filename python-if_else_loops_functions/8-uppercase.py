6f5dc2ee66ef2085b96953e08847a2318e4335d9
DSTV <9871@holbertonstudents.com>
2025-01-14 15:37:58 +1100

    Task Complete

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(str):
     """ Write a function that prints a string in uppercase followed by a new line. """
     
     for i in str:
-        if (96 < ord(i) and ord(i) < 123):
+        if (ord(i) > 96 and ord(i) < 123):
             i = chr(ord(i) - 32)
         print('{0}'.format(i), end="")
     print("")
