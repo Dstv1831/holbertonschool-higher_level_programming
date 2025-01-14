a0f0286532b0bbe0ff145e617a6b199205ce652a
DSTV <9871@holbertonstudents.com>
2025-01-14 17:19:41 +1100

    Task Complete

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import args
+
+num = len(args.argv)
+if num == 0:
+    print("{num} arguments.". format(num))
+elif num == 1:
+    print("{num} argument:". format(num))
+else:
+    print("{num} arguments:". format(num))
+
+for i in range (1, args.argv + 1):
+    print("{0}: {1}". format(i, args.arg[i]))
