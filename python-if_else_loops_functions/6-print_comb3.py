36eb72289a2038f0acb788a58254a262cd6d9905
DSTV <9871@holbertonstudents.com>
2025-01-14 11:36:44 +1100

    Task Complete

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,9 @@
 u = 0
 while u < 8:
     for d in range(0,10):
-        if u == d or :
+        if u == d or u > d :
             continue
         print(f"{u}{d}, ", end="")
     u += 1
-print(f"{n}")
+print(f"{u}{d}")
+
