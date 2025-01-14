50a5d542a59d26d6fe2948a4829235fc40c6390c
DSTV <9871@holbertonstudents.com>
2025-01-14 14:12:32 +1100

    Task Complete

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,4 +4,4 @@ def print_last_digit(number):
     """ Write a function that prints the last digit of a number. """
     ld = abs(number) % 10 
     print(ld, end='')
-    return(ld)
+    return (ld)
