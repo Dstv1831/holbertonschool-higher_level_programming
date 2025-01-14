635810435b95a890c4b27c0af7a3b6ffb8a95558
DSTV <9871@holbertonstudents.com>
2025-01-14 14:42:13 +1100

    Task Complete

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    res = 0
-    a = abs(a)
-    for i in range (0,b):
-        res += a
-    if b % 2 != 0:
-        res = res * -1
+    res = 1
+    for i in range (0, b):
+        res = res * a
     return (res)
 
-pow(2,2)
