e9756a0c9ccbf6adb442f2b820ffe962f6abae2b
DSTV <9871@holbertonstudents.com>
2025-01-20 18:12:34 +1100

    Task Complete

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+def square_matrix_simple(matrix=[]):
+    
+    """Calculates the square of each item inside a matrix"""
+
+    new_matrix = [[item ** 2 for item in row] for row in matrix]
+    #Nested list comprehension 
+    return new_matrix
