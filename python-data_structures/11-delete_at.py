ee23d0d02caaed1e471c57c981011dac316c80a5
DSTV <9871@holbertonstudents.com>
2025-01-20 15:38:00 +1100

    Task Complete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,15 +2,8 @@
 
 def delete_at(my_list=[], idx=0):
     """Deletes the item at the idx position"""
-    new_list = []
     if 0 <= idx < len(my_list):
-        del new_list[idx]
-        return new_list
+        del my_list[idx]
+        return my_list
     else:
         return my_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
