a5153d0f409807fcc5a2087840e5f5e8f4af4a70
DSTV <9871@holbertonstudents.com>
2025-01-20 18:35:39 +1100

    Task Complete

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@
 def search_replace(my_list, search, replace):
 
     """Replaces all occurrences of an element by another"""
+    
     new_list = list(map(lambda x: replace if x == search else x, my_list))
+    # In Python's ternary expression syntax:
+    # # (value_if_true) if condition else (value_if_false)
     return new_list
-
-# In Python's ternary expression syntax:
-# (value_if_true) if condition else (value_if_false)
