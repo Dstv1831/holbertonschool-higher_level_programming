82bb1f4384999c83a8e8cefcd9553420fe5b0db7
DSTV <9871@holbertonstudents.com>
2025-01-20 12:12:00 +1100

    Task Complete

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     """Retrieves the elements at position idx"""
-    if 0 < idx < len(my_list) - 1:
+    if 0 <= idx < len(my_list):
         return my_list[idx]
     else:
         return 0
---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    b = my_list.copy()
+    if 0 <= idx < len(my_list):
+        b[idx] = element
+        return b
+    else:
+        return b
