f2f30c6958ac4b9dc3f12edc155d954dca5796f4
DSTV <9871@holbertonstudents.com>
2025-01-20 15:19:25 +1100

    Task Complete

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = ("True" if item % 2 == 0 else "False" for item in my_list)
+    """Finds all multiples of 2"""
+    new_list = ["True" if item % 2 == 0 else "False" for item in my_list]
     return new_list
 
 my_list = [0, 1, 2, 3, 4, 5, 6]
