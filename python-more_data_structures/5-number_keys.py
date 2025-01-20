c2fb35eb73f030e726eafcb69db89473af4a01c1
DSTV <9871@holbertonstudents.com>
2025-01-20 19:27:13 +1100

    Task Complete

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
-    counter = 0
-    for i in list(a_dictionary):
-        counter += 1
-    return counter
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
