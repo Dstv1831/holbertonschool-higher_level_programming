451b6f299d624f3341da25ebc861d878a0173d14
DSTV <9871@holbertonstudents.com>
2025-01-20 13:03:29 +1100

    Task Complete

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     """Printing a Matrix of integeres"""
+    l = len(matrix)
     for i in matrix:
-        for j in range (3):
-            if j != 2:
-                print("{:d}".format(i[j]), end =' ')
-            else:
-                print("{:d}".format(i[j]))
+        for j in range (l):
+                print("{:d}".format(i[j]), end ='')
+                if j != (l):
+                     print(" ", end='')
         print()
             
