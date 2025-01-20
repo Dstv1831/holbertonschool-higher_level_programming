9e9ae5bda48bd34437ccefa5c44478d0a6fc3e0d
DSTV <9871@holbertonstudents.com>
2025-01-20 12:49:17 +1100

    Task Complete

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
-        return my_list[idx]
+        return my_list
     else:
         return my_list
