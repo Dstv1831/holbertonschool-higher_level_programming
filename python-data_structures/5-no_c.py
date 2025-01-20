e29349e7af87a45d0537227b798196def37f0f98
DSTV <9871@holbertonstudents.com>
2025-01-20 13:37:22 +1100

    Task Complete

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     """Eliminates the C character in a string"""
     """Using list comprehension Conditional"""
     new_string = ''.join([char for char in my_string if char.lower() != 'c'])
-    return(new_string)
+    return (new_string)
