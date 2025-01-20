b771983ce890f5cb5fc498214218551dde98ef00
DSTV <9871@holbertonstudents.com>
2025-01-20 12:22:26 +1100

    Task Complete

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+def no_c(my_string):
+    """Eliminates the C character in a string"""
+    for i in my_string:
+        if i == 'c':
+            continue
+        print(i, end='')
+    print()
