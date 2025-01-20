5c6666aeb5f742ddbe9994362bea3cdc0bcd0aca
DSTV <9871@holbertonstudents.com>
2025-01-20 15:07:29 +1100

    Task Complete

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,11 +2,11 @@
 
 def max_integer(my_list=[]):
     """Find the biggest integer of a list"""
-    highest = 0
-    if len(my_list)==0:
+    hv = my_list[0]
+    if len(my_list) == 0:
         return "None"
     else:
         for i in my_list:
-            if i < i + 1:
-                highest = i + 1
-        return highest
+            if i > hv:
+                hv = i
+        return hv
