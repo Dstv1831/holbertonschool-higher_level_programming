8b469fd115621c17129d9ac2fb951e80e90eff0e
DSTV <9871@holbertonstudents.com>
2025-01-20 12:49:49 +1100

    Task Complete

--- python-data_structures/3-print_reveresed_list_integer.py
+++ python-data_structures/3-print_reveresed_list_integer.py
@@ -4,7 +4,4 @@ def print_reversed_list_integer(my_list=[]):
     """Prints the elements of a list in reverse"""
     my_list.reverse()
     for i in my_list:
-        print("{}".format(i))
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
+        print("{:d}".format(i))
