c5e0d0f775c1635b10a05167e2547a475ac957c6
DSTV <9871@holbertonstudents.com>
2025-01-20 19:02:26 +1100

    Task Complete

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    """adds all unique integers in a list (only once for each integer"""
     new_list = set(my_list)
     total = sum(new_list)
     return total
-    
+ 
