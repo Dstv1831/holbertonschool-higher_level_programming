560fe45acd9309e5aa5a87e05bfec44d0d8d36d0
DSTV <9871@holbertonstudents.com>
2025-01-20 12:44:39 +1100

    Task Complete

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Printing a Matrix of integeres"""
+    for i in matrix:
+        for j in i:
+            print(j, end =' ')
+        print()
+            
