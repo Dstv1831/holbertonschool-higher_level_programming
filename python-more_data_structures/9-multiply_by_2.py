b4e4486118d4d4b06b9e361d5a42c770329c6bc2
DSTV <9871@holbertonstudents.com>
2025-01-21 10:38:34 +1100

    Task Complete

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 
 def multiply_by_2(a_dictionary):
     """Multiply every item on the dictionary by 2"""
-    new_dict = {k: v * 2 for (k,v) in a_dictionary.items()}
+    new_dict = {k: v * 2 for (k, v) in a_dictionary.items()}
     return new_dict
