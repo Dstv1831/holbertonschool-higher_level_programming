c382b4613f3108a05b46be8a7eb1faf2858d9b69
DSTV <9871@holbertonstudents.com>
2025-01-20 18:34:58 +1100

    Task Complete

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+
+    """Replaces all occurrences of an element by another"""
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
 
