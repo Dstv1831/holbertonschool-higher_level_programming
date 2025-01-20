6d6464ecd6ba0d7351e97c601c6f043c352fd13a
DSTV <9871@holbertonstudents.com>
2025-01-20 19:47:33 +1100

    Task Complete

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    dict = sorted(a_dictionary)
-    dict = dict(dict)
-    return print(dict)
+    """Print a dictionary by ordered keys."""
+    [print("{}: {}".format(items, a_dictionary[items])) for items in sorted(a_dictionary)]
