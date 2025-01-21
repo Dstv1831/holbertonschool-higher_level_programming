a0aa3c03a43de445921d1c1bcea979ceb63a01eb
DSTV <9871@holbertonstudents.com>
2025-01-21 14:25:34 +1100

    Task Complete

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    
     """Function that prints x elements of a list"""
+
     try:
-        list = [my_list[i] for i in range(x)]
-        count += 1
+        for i in range(x):
+            print('{}'.format(my_list[i]), end="")
+            count += 1
     except IndexError:
-        list = [item for item in my_list]
+        pass
+    print()
     return count
