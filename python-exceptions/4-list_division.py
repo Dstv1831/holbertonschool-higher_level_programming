f9cda7423b8a7662e250ee79be2f9ae6c4855272
DSTV <9871@holbertonstudents.com>
2025-01-21 16:59:11 +1100

    Task Complete

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -19,8 +19,3 @@ def list_division(my_list_1, my_list_2, list_length):
             else:
                 new.append(0)
     return new
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
