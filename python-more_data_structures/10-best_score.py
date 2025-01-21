c99539622c32b95ee580559c5f6763cb05f71cfe
DSTV <9871@holbertonstudents.com>
2025-01-21 11:06:37 +1100

    Task Complete

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,8 +4,9 @@ def best_score(a_dictionary):
 
     """Find the highest score in a dcitionary"""
 
-    if len(a_dictionary) != 0:
+    if a_dictionary:
         max_val = max(a_dictionary, key = a_dictionary.get)
         return max_val
     else:
         return "None"
+
