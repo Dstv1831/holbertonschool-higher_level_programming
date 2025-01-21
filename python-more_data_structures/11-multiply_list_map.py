cf8583455dfafb7a24587b3d62814fb37ba27694
DSTV <9871@holbertonstudents.com>
2025-01-21 11:16:09 +1100

    Task Complete

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
     """Create a new list with all its value multiply by a number"""
-    new_list = list(map(lambda x: x*number, my_list))
-    return new_list
+    return list(map(lambda x: x*number, my_list))
