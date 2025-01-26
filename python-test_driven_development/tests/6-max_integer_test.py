5450c1540593b8183acc48efa31ab7dd31b76b5b
DSTV <9871@holbertonstudents.com>
2025-01-26 22:01:39 +1100

    Task Complete

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,31 @@ class TestMaxInteger(unittest.TestCase):
           """Test a list with a max value in the last element"""
           max_at_end = [1, 2, 3, 4]
           self.assertEqual(max_integer(max_at_end), 4)
+     
+     def test_max_at_the_begining(self):
+          """Test a list with a max value in the first element"""
+          max_at_beginning = [3, 2, 1]
+          self.assertEqual(max_integer(max_at_beginning), 3)
+     
+     def test_max_in_the_middle(self):
+          """Test a list with a max value in the middle"""
+          max_middle = [1, 2, 5, 4, 3]
+          self.assertEqual(max_integer(max_middle), 5)
+     
+     # def test_max_at_the_end(self):
+     #      """Test a list with a max value in the last element"""
+     #      max_at_end = [1, 2, 3, 4]
+     #      self.assertEqual(max_integer(max_at_end), 4)
+     
+     # def test_max_at_the_end(self):
+     #      """Test a list with a max value in the last element"""
+     #      max_at_end = [1, 2, 3, 4]
+     #      self.assertEqual(max_integer(max_at_end), 4)
+     
+     # def test_max_at_the_end(self):
+     #      """Test a list with a max value in the last element"""
+     #      max_at_end = [1, 2, 3, 4]
+     #      self.assertEqual(max_integer(max_at_end), 4)
           
      if __name__ == '__main__':
           unittest.main()
