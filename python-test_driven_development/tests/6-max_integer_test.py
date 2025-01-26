ea2450a505984558e4da1fa5a793c21d9993f10f
DSTV <9871@holbertonstudents.com>
2025-01-26 21:53:35 +1100

    Task Complete

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,16 @@
 import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+
      """Define unittests for max_integer([..])."""
+     
+     def test_max_at_the_end(self):
+          """Test a list with a max value in the last element"""
+          max_at_end = [1, 2, 3, 4]
+          self.assertEqual(max_integer(max_at_end), 4)
+
+
 
 if __name__ == '__main__':
     unittest.main()
