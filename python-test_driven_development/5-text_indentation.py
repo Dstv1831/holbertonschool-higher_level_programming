764150754a2a7e042b1efd7f4d0f74ad53ce141f
DSTV <9871@holbertonstudents.com>
2025-01-26 13:56:56 +1100

    Task Complete

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
+"""
+5-text_indentation.py
+
+This module provides a function that prints a text
+with two new lines after encountering the characters:
+":", ".", "?" 
+"""
+
 
 def text_indentation(text):
+    """
+    Prints a text with a new line indented 
+    if any of the characters ":", ".", "?" is found
+
+    Raises:
+    TypeError: If the argument is not a string
+    """
+    i = 0
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    while(text):
-        i = 0
-        print(text[i])
+    while(i < len(text)):
+        if text[i] != '.' and text[i] != '?' and text[i] !=':':
+            print(text[i], end='')
+        else:
+            print('\n')
         i += 1
-
-text_indentation("Konbanwa david san")
