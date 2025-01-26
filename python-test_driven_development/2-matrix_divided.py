e92fa8ce3b9f892baa20adfadd93676153ff6ee6
DSTV <9871@holbertonstudents.com>
2025-01-26 12:01:16 +1100

    Task Complete

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -26,13 +26,14 @@ def matrix_divided(matrix, div):
         [num for row in matrix for num in row] means:
         for each row on matrix (outer loop)
         for each num on row (inner loop)
-        value will be num 
+        value will be num
     """
     if (not isinstance(matrix, list) or matrix == [] or
         not all(isinstance(row, list) for row in matrix) or
         not all((isinstance(ele, int) or isinstance(ele, float))
                 for ele in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     if not all([len(row) == len(matrix[0]) for row in matrix]):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, int) and not isinstance(div, float):
