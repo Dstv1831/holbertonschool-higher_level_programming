7c9dc2a4158a1370cc6b68b7219cd637f0998b06
DSTV <9871@holbertonstudents.com>
2025-01-26 13:35:30 +1100

    Task Complete

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -19,7 +19,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    for i in range (size):
-        for j in range (size):
+    for i in range(size):
+        for j in range(size):
             print("#", end='')
         print("")
