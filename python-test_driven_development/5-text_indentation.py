4321cdebde34bfb25a3c30c74ceab00bae962ce8
DSTV <9871@holbertonstudents.com>
2025-01-26 14:12:52 +1100

    Task Complete

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,13 +4,13 @@
 
 This module provides a function that prints a text
 with two new lines after encountering the characters:
-":", ".", "?" 
+":", ".", "?"
 """
 
 
 def text_indentation(text):
     """
-    Prints a text with a new line indented 
+    Prints a text with a new line indented
     if any of the characters ":", ".", "?" is found
 
     Raises:
