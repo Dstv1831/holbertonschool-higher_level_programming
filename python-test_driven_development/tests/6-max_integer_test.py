ebeadaa24efce41a94ff4a85a81a9c9d3dc971ab
DSTV <9871@holbertonstudents.com>
2025-01-26 22:09:11 +1100

    Task Complete

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -21,19 +21,24 @@ class TestMaxInteger(unittest.TestCase):
           self.assertEqual(max_integer(max_middle), 5)
      
      def test_negative_number(self):
-          """Test a list with a mnegative value number"""
+          """Test a list with a negative value number"""
           max_negative = [1, 2, -3, 4, -5]
           self.assertEqual(max_integer(max_negative), 4)
      
      def test_only_negative(self):
-          """Test a list with a max value in the last element"""
+          """Test a list with only negative numbers"""
           max_only_negative = [-8, -3, -2, -1, -5]
           self.assertEqual(max_integer(max_only_negative), -1)
      
      def test_one_element(self):
-          """Test a list with a max value in the last element"""
-          max_one = [45]
+          """Test a list with only one element"""
+          max_one = [90]
           self.assertEqual(max_integer(max_one), 45)
+
+     def test_empty(self):
+          """Test a list with no elements"""
+          max_one = []
+          self.assertEqual(max_integer(max_one), 0)
           
      if __name__ == '__main__':
           unittest.main()
