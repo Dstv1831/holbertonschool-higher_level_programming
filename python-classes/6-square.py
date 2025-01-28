2cd6e2c95bb3df48b40e9b285f29b609c3c46121
DSTV <9871@holbertonstudents.com>
2025-01-28 14:19:53 +1100

    Task Complete

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -60,6 +60,8 @@ class Square:
             print()
         else:
             for i in range(self.__size):
+                for x in range (self.__position[0]):
+                    print(' ')
                 for j in range(self.__size):
                     print("#", end="")
                 print()
