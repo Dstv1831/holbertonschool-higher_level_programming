0ef2df7edecf3434083e37da165ede412becc311
DSTV <9871@holbertonstudents.com>
2025-01-28 15:14:49 +1100

    Task Complete

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -60,8 +60,8 @@ class Square:
     def position(self, coordinates):
         """Decorator for a setter to adjust the value of the position attribute"""
         if (not isinstance(coordinates, tuple) or 
-            not all((coord >= 0) for coord in coordinates) or
-            not all(isinstance(coord, int) for coord in coordinates)):
+            not all(isinstance(coord, int) for coord in coordinates) or
+            not all((coord >= 0) for coord in coordinates)):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = coordinates
