09958786f4f5009c720e8168422a5d2685d2c09c
DSTV <9871@holbertonstudents.com>
2025-01-28 11:04:19 +1100

    Task Complete

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+0-square
+This module contains the  class square
+"""
+
+
+class Square:
+    """
+    class square
+
+    attributes:
+        size:
+    """
+    def __init__(self, size=0):
+        """Private instance attribue"""
+        if not isinstance (size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
