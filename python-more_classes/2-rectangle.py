66dd23a39aa1b56bf42c33001d631e095a73abe5
DSTV <9871@holbertonstudents.com>
2025-01-30 09:16:05 +1100

    Task Complete

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
     def area(self):
         """public method that return the area"""
         return (self.height * self.width)
-    
+
     def perimeter(self):
         """public method that return the area"""
         if self.width == 0 or self.height == 0:
