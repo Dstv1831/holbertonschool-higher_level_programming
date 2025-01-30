520dae8c5fb3f3c9e326850b4407307248c16220
DSTV <9871@holbertonstudents.com>
2025-01-30 14:55:40 +1100

    Task Complete

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -96,4 +96,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """class method that returns a new Rectangle instance"""
-        return(cls(width=size, height=size))
+        return (cls(width=size, height=size))
