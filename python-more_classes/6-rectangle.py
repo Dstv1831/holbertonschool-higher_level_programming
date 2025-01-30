5e407c4812a4faacfc710cd411b424d47b3521bb
DSTV <9871@holbertonstudents.com>
2025-01-30 13:35:21 +1100

    Task Complete

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -77,5 +77,5 @@ class Rectangle:
 
     def __del__(self):
         """Delete the rectangle instance """
-        self.number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
