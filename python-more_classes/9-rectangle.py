fde058030e25c7b62941b36145e8ce262c358044
DSTV <9871@holbertonstudents.com>
2025-01-30 14:53:10 +1100

    Task Complete

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -92,3 +92,8 @@ class Rectangle:
         if (rect_2.area() > rect_1.area()):
             return rect_2
         return rect_1
+
+    @classmethod
+    def square(cls, size = 0):
+        """class method that returns a new Rectangle instance"""
+        return(cls(width=size, height=size))
