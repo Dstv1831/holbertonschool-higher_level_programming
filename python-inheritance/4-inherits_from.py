ef2c978a38125bf8d0bb14dbdfa987790952e973
DSTV <9871@holbertonstudents.com>
2025-02-04 10:44:58 +1100

    Task Complete

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@ is an instance of a class that inherited
 (directly or indirectly) from the specified class"""
 
 
-def inherists_from(obj, a_class):
+def inherits_from(obj, a_class):
     """Function that returns True if the object
     is exactly an instance of the specified class;
     otherwise False."""
