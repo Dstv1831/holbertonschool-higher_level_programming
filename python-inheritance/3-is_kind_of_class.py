69d0972dfd91964ce7cefbaa7c658951a2a7800d
DSTV <9871@holbertonstudents.com>
2025-02-04 10:37:23 +1100

    Task Complete

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module contains a function that chcks for instances of same class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns True if the object
+    is exactly an instance of the specified class;
+    otherwise False."""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
