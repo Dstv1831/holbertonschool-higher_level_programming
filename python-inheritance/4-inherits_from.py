6ea8234673d35045b5275d70168c9a65065f7453
DSTV <9871@holbertonstudents.com>
2025-02-04 10:52:03 +1100

    Task Complete

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     is exactly an instance of the specified class;
     otherwise False."""
 
-    if issubclass(type(obj), a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
