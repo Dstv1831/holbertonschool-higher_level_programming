e07b0213d93e64392bf42c84a311345d3070d76e
DSTV <9871@holbertonstudents.com>
2025-02-04 09:47:49 +1100

    Task Complete

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Class that inherist from the object list"""
+
+class MyList(list):
+    """MyList that inherits from list"""
+
+    def print_sorted(self):
+        """Public instance Method that prints the list but sorted"""
+        return sorted(self)
