cd84da24e4c7e35458a3d9225a93885c25f0e41f
DSTV <9871@holbertonstudents.com>
2025-02-04 10:35:40 +1100

    Task Complete

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-"""This module contains a function that chcks for instances of same classs"""
+"""This module contains a function that chcks for instances of same class"""
 
 
-def is_kind_of_class(obj, a_class):
+def is_same_class(obj, a_class):
     """Function that returns True if the object
     is exactly an instance of the specified class;
     otherwise False."""
 
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
