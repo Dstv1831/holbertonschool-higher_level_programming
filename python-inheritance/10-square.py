3c6cceaa83b1976ec7544ce850d77b2488398791
DSTV <9871@holbertonstudents.com>
2025-02-04 15:01:47 +1100

    Task Complete

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -16,7 +16,7 @@ class Square(Rectangle):
 
         self.integer_validator("size", size)
         self.__size = size
-    
+
     def area(self):
         """Public instance method that calculates the Area"""
         return pow(self.__size, 2)
