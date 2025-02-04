fc4a925d0f9a6ce02e69e50b11e3a268be48f0de
DSTV <9871@holbertonstudents.com>
2025-02-04 11:23:43 +1100

    Task Complete

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module containing an empty class"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry that inherits from list"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
