784d12eb175bcba240338c00f0dd1664d4d46e80
DSTV <9871@holbertonstudents.com>
2025-02-04 15:00:38 +1100

    Task Complete

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a class Square that inherits from Rectangle."""
 
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -16,3 +16,7 @@ class Square(Rectangle):
 
         self.integer_validator("size", size)
         self.__size = size
+    
+    def area(self):
+        """Public instance method that calculates the Area"""
+        return pow(self.size, 2)
