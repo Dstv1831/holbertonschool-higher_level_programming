a99c21df96bcf7ef6211f073a57929cedebfc502
DSTV <9871@holbertonstudents.com>
2025-02-09 14:56:28 +1100

    Task Complete

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" Module that contains the "Dragon" Class, which
+    inherits from two mixin classes 
+    "SwimMixin" & "FlyMixin"
+"""
+class SwimMixin:
+    
+    def swim(self):
+        return print("The creature swims!")
+    
+class FlyMixin:
+    
+    def fly(self):
+        return print("The creature flies")
+
+    
+class Dragon(SwimMixin, FlyMixin):
+    
+    """
+    Class Dragon that inherits from both
+    a SwimMixin and a FlyMixin class. 
+
+    Attributes:
+        Name: name of the new object of class Dragon 
+    Methods:
+        Roar: Returns "<name> roars!"
+    """ 
+
+    def __init__(self, name):
+        self.Name = name
+    
+    def roar(self):
+        print(f"{self.Name} roar !!!")
