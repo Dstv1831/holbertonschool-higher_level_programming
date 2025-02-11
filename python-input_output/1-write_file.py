e4b8fd59780614bf56e41b5bd8b1fc8d60d1cad9
DSTV <9871@holbertonstudents.com>
2025-02-12 09:14:57 +1100

    Task Complete

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -13,14 +13,5 @@ def write_file(filename = "", text = ""):
     Function that opens a file, writes on 
     it and print the number of characters
     """
-
-    with open(file = filename, mode = "r+", encoding = "utf-8") as myfile:
-        myfile.write(text)
-        content_list = (myfile.read()).split
-
-    counter = 0
-    for words in content_list:
-        for character in words:
-            counter += 1
-    
-    return counter
+    with open(file = filename, mode = "w", encoding = "utf-8") as myfile:
+        return (myfile.write(text))
