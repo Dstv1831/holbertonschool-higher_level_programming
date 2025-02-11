b7faea1d26f24d04b775c3d2ac9ca589ffde7788
DSTV <9871@holbertonstudents.com>
2025-02-12 09:47:19 +1100

    Task Complete

--- python-input_output/3-to_jason_string.py
+++ python-input_output/3-to_jason_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-"""
-This module contains a function that returns
-the JSON representation of an object
+"""This module contains a function that returns
+    the JSON representation of an object
 """
 
 import json
