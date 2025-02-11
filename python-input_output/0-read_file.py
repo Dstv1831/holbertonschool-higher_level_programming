6935e6a7bbab411aea9e73c12001979c71ae737e
DSTV <9871@holbertonstudents.com>
2025-02-12 08:53:01 +1100

    Task Complete

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ This module contains a function that opens a file
 reads and print its content in stdout
 """
 
+
 def read_file(filename=""):
 
     "Function that opens a file and print its content into stdout"
