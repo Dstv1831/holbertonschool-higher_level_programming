c61e419405e679f901c0c3f383c757601aeff6d9
DSTV <9871@holbertonstudents.com>
2025-02-12 10:42:14 +1100

    Task Complete

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,5 +13,5 @@ def save_to_json_file(my_obj, filename):
     writes an Object to a text file, using a JSON representation
     """
     json_encoded = json.dumps(my_obj)
-    with open(file=filename,mode="w",encoding="utf-8") as myfile:
-        myfile.write(json_encoded)
+    with open(file=filename, mode="w", encoding="utf-8") as myfile:
+        json.dump(json_encoded, myfile)
