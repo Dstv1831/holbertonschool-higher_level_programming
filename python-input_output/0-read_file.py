bb843983ecdc5ea1a4467d0d12a86d48e12ac0b6
DSTV <9871@holbertonstudents.com>
2025-02-12 08:48:31 +1100

    Task Complete

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     "Function that opens a file and print its content into stdout"
 
     with open(file=filename, encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
