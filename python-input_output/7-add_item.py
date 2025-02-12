07804c4304ab64c0e860bd7059cdb1f1df3ac76d
DSTV <9871@holbertonstudents.com>
2025-02-12 11:49:31 +1100

    Task Complete

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,5 +12,5 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     load_from_json_file("add_item.json")
-finally:
+except FileNotFoundError:
     save_to_json_file(sys.argv, "add_item.json")
