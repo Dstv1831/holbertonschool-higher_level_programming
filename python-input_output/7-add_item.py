eb243c7343dccaa3e6518fe28c8f6c8f735e5a26
DSTV <9871@holbertonstudents.com>
2025-02-13 09:15:32 +1100

    Task Complete

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -15,5 +15,5 @@ try:
     vector = load_from_json_file("add_item.json")
 except FileNotFoundError:
     vector = []
-vector.append(sys.argv)
+vector.extend(sys.argv[1:])
 save_to_json_file(vector, "add_item.json")
