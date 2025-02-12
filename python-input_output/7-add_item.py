9085f7e7807910ee5828a2783e803964006c70ce
DSTV <9871@holbertonstudents.com>
2025-02-12 11:26:01 +1100

    Task Complete

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ import json
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-save_to_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 with open (file="add_item.json", mode="w", encoding="utf-8") as myfile:
     json.dump(sys.argv, myfile)
