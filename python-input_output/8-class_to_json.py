9d55820f80e3c6aee77e76b99a316111862f5787
DSTV <9871@holbertonstudents.com>
2025-02-13 09:38:36 +1100

    Task Complete

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function that returns the
+dictionary description of a simple data structure
+for JSON serialization of an object
+"""
+
+
+def class_to_obj(obj):
+    
+    """
+    returns de dictionary description of a class
+    """
+    return obj.__dict__
