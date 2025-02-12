2df9cfed2267b80e1a71bc5854bede9182b65aba
DSTV <9871@holbertonstudents.com>
2025-02-12 11:53:54 +1100

    Task Complete

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,14 +5,15 @@ This module contains a script that adds all the stdin arguments
 to a Python list, and then save them to a file
 """
 
-if __name__ == "__main__":
-    
-    import sys
 
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
 
-    try:
-        load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        save_to_json_file(sys.argv, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    vector = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    vector = []
+vector = sys.argv
+save_to_json_file(vector, "add_item.json")
