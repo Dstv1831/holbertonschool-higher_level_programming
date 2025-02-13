030315ffc3ef160f014fab5824e1316eaa8f2064
DSTV <9871@holbertonstudents.com>
2025-02-13 13:50:07 +1100

    Task Complete

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -37,6 +37,7 @@ class Student:
             return self.__dict__
         
     def reload_from_json(self, json):
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        if json is not None:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]
