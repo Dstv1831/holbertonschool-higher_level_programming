d8715406f4b43167867bf6a8a726770cdb28a431
DSTV <9871@holbertonstudents.com>
2025-02-13 13:49:03 +1100

    Task Complete

---
+++ python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+This module contains a Class Student with a public
+method that retrieves a dictionary representation
+of a student instance and the specified attributes
+"""
+
+
+class Student:
+    """
+    Class that creates a student
+
+    attributes:
+        public instance first_name
+        public instance last_name
+        public instance age
+
+    public methods:
+        to_jason: Return the specified attributes value
+        reload_from_jason: replaces all attributes of the Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        if attr is not None:
+            new_dict = {key: self.__dict__[key]
+                        for key in self.__dict__
+                        if (key in attr)}
+            return new_dict
+
+        else:
+            return self.__dict__
+        
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
