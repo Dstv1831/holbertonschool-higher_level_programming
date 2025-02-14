d6d1cbba7f00a8cb441bf163ca6c48287ca57c42
DSTV <9871@holbertonstudents.com>
2025-02-14 14:49:09 +1100

    Task Complete

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -12,9 +12,14 @@ def serialize_and_save_to_file(data, filename):
 
     with open(file=filename, mode="w", encoding="utf-8") as myfile:
         json.dump(data, myfile)
+        print (data)
 
 
 def load_and_deserialize(filename):
 
     with open(file=filename, mode="r", encoding="utf-8") as myfile:
-        json.load(myfile)
+        new_data=json.load(myfile)
+        print (new_data)
+
+
+
