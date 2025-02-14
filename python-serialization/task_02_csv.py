52edb1549f0bbd93b5637d48997abbe165719840
DSTV <9871@holbertonstudents.com>
2025-02-14 16:36:39 +1100

    Task Complete

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module contain an function that serializes
+data from a CSV file to JSON
+"""
+
+import csv
+import json
+
+def convert_csv_to_json(csv_file):
+    try:
+        with open(file=csv_file, mode="r", encoding="utf-8") as csvf:
+            csvreader = csv.DictReader(csvf)
+
+            for rows in csvreader:
+                json.dump(rows,"data.json")
+    except FileNotFoundError:
+        return False
+    return True
