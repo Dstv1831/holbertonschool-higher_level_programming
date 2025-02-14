493b20a7f046443f02bac02db44637a0c6fc18c2
DSTV <9871@holbertonstudents.com>
2025-02-14 16:47:31 +1100

    Task Complete

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -9,11 +9,15 @@ import csv
 import json
 
 def convert_csv_to_json(csv_file):
+    data_list = []
     try:
         with open(file=csv_file, mode="r+", encoding="utf-8") as csvf:
             csvreader = csv.DictReader(csvf)
             for rows in csvreader:
-                json.dump(rows,"data.json")
+                data_list.append(rows)
     except FileNotFoundError:
         return False
+    
+    with open(file="json.data", mode="w", encoding="utf-8") as jsonf:
+        json.dump(data_list, jsonf)
     return True
