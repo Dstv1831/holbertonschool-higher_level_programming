040260b7fd817f04e54438d6e8867ff0ee052913
DSTV <9871@holbertonstudents.com>
2025-02-18 16:39:35 +1100

    Task Complete

--- resful-api/task_02_requests.py
+++ resful-api/task_02_requests.py
@@ -2,16 +2,26 @@
 
 import requests
 
-r = requests.get("https://jsonplaceholder.typicode.com/")
+
 
 def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')    
     status = r.status_code
-    print(status)
     if status == 200:
         data = r.json()
-        print(data)
+        for item in data:
+            print(item["title"])
 
 def fetch_and_save_posts():
-    print()
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')    
+    status = r.status_code
+    if status == 200:
+        new_data = []
+        data = r.json()
+        for each in data:
+            new_dict = {key:value for key, value in each.items() if key == "id"}
+            print (new_dict)
 
-fetch_and_print_posts()
+
+
+fetch_and_save_posts()
