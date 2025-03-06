893bb6721f09a320c3bf8bbe5cad2a3b5936790d
DSTV <9871@holbertonstudents.com>
2025-03-06 21:00:54 +1100

    Task Complete

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- This script list "score" and "name" of the second_table 
+-- in order (Top first) the database hbtn_0c_0 
+-- in MySQL server and add multiple rows
+
+SELECT (id, name) from second_table ORDER BY id ASC | DESC;
+
+
