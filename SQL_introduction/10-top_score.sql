83d8138b9b7b9b1f6c7da5a2cdfd78859c6ad5fd
DSTV <9871@holbertonstudents.com>
2025-03-06 21:05:51 +1100

    Task Complete

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -2,6 +2,6 @@
 -- in order (Top first) the database hbtn_0c_0 
 -- in MySQL server and add multiple rows
 
-SELECT (score, name) FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
 
 
