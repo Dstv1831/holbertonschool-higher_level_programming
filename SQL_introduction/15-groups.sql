d6526661e53025ba43a5a992c99a6d675594710e
DSTV <9871@holbertonstudents.com>
2025-03-07 01:29:08 +1100

    Task Complete

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- This script lists the number of records with 
 -- the same score from the second_table of MySQL server
 
+GROUP BY score
 SELECT score, COUNT(*) AS number FROM second_table
-GROUP BY score;
 ORDER BY number DESC;
