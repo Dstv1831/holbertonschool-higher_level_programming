0ab2c6f49fb435dc3552b46a6e79fa0098632207
DSTV <9871@holbertonstudents.com>
2025-03-07 20:55:28 +1100

    Task Complete

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- and password set to user_0d_1_pwd
 
 CREATE USER 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
 FLUSH PRIVILEGES;
