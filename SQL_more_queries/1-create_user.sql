1ba2accb31a7ad4f8fb05c9bb4557f5d71cded34
DSTV <9871@holbertonstudents.com>
2025-03-07 20:57:37 +1100

    Task Complete

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Creates the MySQL User user_0d_1 with all privileges
 -- and password set to user_0d_1_pwd
 
-CREATE USER 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
