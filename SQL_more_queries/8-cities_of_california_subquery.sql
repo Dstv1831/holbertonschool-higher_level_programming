54d6cc7d81b5ad49dd9bd3ad74b82a88ccc8c37f
DSTV <9871@holbertonstudents.com>
2025-03-08 01:29:34 +1100

    Task Complete

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- lists all the cities of California that 
 -- can be found in the database hbtn_0d_usa.
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name
