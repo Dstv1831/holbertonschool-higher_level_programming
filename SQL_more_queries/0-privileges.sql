11e781cdadaa8a8cb234f23a533a88bf5d3a268d
DSTV <9871@holbertonstudents.com>
2025-03-07 20:45:02 +1100

    Task Complete

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- lists all the privileges of the MySQL Users
 -- user_0d_1 & user_0d_2 on the server in (localhost)
 
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
