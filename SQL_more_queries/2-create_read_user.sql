2918e3059d167758577e304e9194b04f1c2fd8bc
DSTV <9871@holbertonstudents.com>
2025-03-07 21:05:23 +1100

    Task Complete

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Creates the Database hbtn_0d_2 and User user_0d_2
+-- with only SELECT privileges and password set to user_0d_2_pwd
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
