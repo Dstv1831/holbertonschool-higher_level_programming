e166c57bb049ff5825df8f198ab3a6922dfe19da
DSTV <9871@holbertonstudents.com>
2025-03-08 20:33:00 +1100

    Task Complete

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,7 @@
 -- can be found in the database hbtn_0d_usa.
 
 SELECT cities.id, cities.name, states.name
-FROM cities, states
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
