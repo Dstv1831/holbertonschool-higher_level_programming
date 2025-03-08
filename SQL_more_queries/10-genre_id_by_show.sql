1da418ab5701e4f513b8ad19504227ed6eeb9060
DSTV <9871@holbertonstudents.com>
2025-03-08 20:36:26 +1100

    Task Complete

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
--- lists all the cities in the database hbtn_0d_usa.
--- use the INNER JOIN to match the common elements of
--- both tables
+-- lists all the cities of California that 
+-- can be found in the database hbtn_0d_usa.
 
 SELECT cities.id, cities.name, states.name
 FROM cities
