6ad5ec9e5b7e4b48c6a32bafe264956d780e4641
DSTV <9871@holbertonstudents.com>
2025-03-08 20:47:37 +1100

    Task Complete

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
--- lists all the cities of California that 
--- can be found in the database hbtn_0d_usa.
+-- lists all the shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked.
 
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states
-ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
