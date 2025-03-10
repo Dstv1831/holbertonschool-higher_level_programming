3f464dd08625d9d6ce1a15cb678d83394f963529
DSTV <9871@holbertonstudents.com>
2025-03-10 13:59:37 +1100

    Task Complete

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,14 @@
 import MySQLdb
 import sys
 
+
 def list_states():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
+    db = MySQLdb.connect (host="localhost",\
+                          user=username, passwd=password, db=database)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY states.id")
