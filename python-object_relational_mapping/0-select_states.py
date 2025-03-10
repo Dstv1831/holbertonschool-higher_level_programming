ac61e945b7e476251fb3fd86f20517dd216ca209
DSTV <9871@holbertonstudents.com>
2025-03-10 13:53:33 +1100

    Task Complete

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+"""This module lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
 def list_states():
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
