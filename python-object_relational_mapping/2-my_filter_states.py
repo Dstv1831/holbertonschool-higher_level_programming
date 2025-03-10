5d8bc0dfe1a48299696f3421a13c7dd6884722ec
DSTV <9871@holbertonstudents.com>
2025-03-10 18:11:31 +1100

    Task Complete

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,12 +12,13 @@ def list_states():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=database, port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name ='{}'ORDER BY states.id ASC".format(state_name))
 
     states = cur.fetchall()
 
