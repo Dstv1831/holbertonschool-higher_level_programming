afbd60c565cceebb70cb1c5c5d7507b501980a24
DSTV <9871@holbertonstudents.com>
2025-03-10 18:58:15 +1100

    Task Complete

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ def list_states():
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=database, port=3306)
     cur = db.cursor()
-    query = "SELECT * FROM cities"
+    query = "SELECT * FROM cities, states"
 
     cur.execute(query)
 
