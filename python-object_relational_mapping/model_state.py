2d2903431e3662bda1f8f03cef0ab8d7f6435099
DSTV <9871@holbertonstudents.com>
2025-03-11 00:31:32 +1100

    Task Complete

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,15 +10,17 @@ from sqlalchemy import (
     Integer)
 from sqlalchemy.ext.declarative import declarative_base
 
+
 #  Define the base class for ORM models
 Base = declarative_base()
 
 class State (Base):
     """State class that links to the MySQL table `states`."""
-    
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
 
 db_url = "sqlite://"
 engine = create_engine(db_url)
