f403d4c08192cc7715e2d670a9bc57a4b124219b
DSTV <9871@holbertonstudents.com>
2025-03-12 00:24:37 +1100

    Task Complete

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,8 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
