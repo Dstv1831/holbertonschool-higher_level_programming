1f3acf7f270c61970ae7b5bb15490fe69da294a1
DSTV <9871@holbertonstudents.com>
2025-03-12 00:14:01 +1100

    Task Complete

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,23 +10,24 @@ from model_state import Base, State
 
 
 def list_states():
+    """List all States Objects """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-db_url = "sqlite://"
-engine = create_engine(db_url)
+    db_url = "sqlite://"
+    engine = create_engine(db_url)
 
-# create a Session
-Session = sessionmaker(bind=engine)
-session = Session()
+    # create a Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-states = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()
 
-for state in states:
-    print(f"{states.id}: {state.name}")
+    for state in states:
+        print(f"{states.id}: {state.name}")
 
-session.close()
+    session.close()
 
 if __name__=="__main__":
     list_states()
