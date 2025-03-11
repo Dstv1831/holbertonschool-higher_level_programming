936f134be5fa2a9a3349208ef8f7594dbd8a38ad
DSTV <9871@holbertonstudents.com>
2025-03-12 00:08:07 +1100

    Task Complete

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Write a script that lists all State objects
+from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+def list_states():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+
+db_url = "sqlite://"
+engine = create_engine(db_url)
+
+# create a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+states = session.query(State).order_by(State.id).all
+
+for state in states:
+    print(f"{states.id}: {state.name}")
+
+session.close()
+
+if __name__=="__main__":
+    list_states()
