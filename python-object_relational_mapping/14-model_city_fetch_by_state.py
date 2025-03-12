4f77c78f9cf86b102791265d08aa1d2e4d7c4e74
DSTV <9871@holbertonstudents.com>
2025-03-12 20:48:24 +1100

    Task Complete

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     cities = session.query(City, State.name).join(State).order_by(City.id).all()
 
     for city, state_name in cities:
-        print(f"{state_name}: ({city.id}) {city.name}") 
-    
+        print(f"{state_name}: ({city.id}) {city.name}")
+
     session.close()
