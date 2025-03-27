21ed9dff3da6aa27006eb821207229e3c58bcb45
DSTV <9871@holbertonstudents.com>
2025-03-27 14:28:22 +1100

    Task Completed

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -2,14 +2,14 @@ import os
 
 file_path='/DSTV/holbertonschool-higher_level_programming/python-server_side_rendering/template.txt'
 
-attendees = [
-    {"name": "Alice", "event_title": "Python Conference", "event_date": "2023-07-15", "event_location": "New York"},
-    {"name": "Bob", "event_title": "Data Science Workshop", "event_date": "2023-08-20", "event_location": "San Francisco"},
-    {"name": "Charlie", "event_title": "AI Summit", "event_date": None, "event_location": "Boston"}
-]
+# attendees = [
+#     {"name": "Alice", "event_title": "Python Conference", "event_date": "2023-07-15", "event_location": "New York"},
+#     {"name": "Bob", "event_title": "Data Science Workshop", "event_date": "2023-08-20", "event_location": "San Francisco"},
+#     {"name": "Charlie", "event_title": "AI Summit", "event_date": None, "event_location": "Boston"}
+# ]
 
-with open('template.txt', 'r') as file:
-    template_content = file.read()
+# with open('template.txt', 'r') as file:
+#     template_content = file.read()
 
 def generate_invitations (template, attendees):
     try:
@@ -35,5 +35,3 @@ def generate_invitations (template, attendees):
                 invitation.write(new_template)
     except (TypeError, ValueError) as e:
         print (f"Error: {e}")
-    
-generate_invitations(template_content, attendees)
