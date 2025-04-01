55cf1a0854b7dcb06e8f035c76a4cfc811eae094
DSTV <9871@holbertonstudents.com>
2025-04-02 09:44:29 +1100

    Task Completed

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -14,5 +15,17 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/items')
+def items():
+
+    with open("items.json") as json_file:
+        data = json.load(json_file)
+
+    return render_template('items.html', json_data = data)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+    # // "experiment_1": [],
+    # // "experiment_2": ["",""],
+    # // "experiment_3": [123, 30.5, true]
